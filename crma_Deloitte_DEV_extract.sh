554321f6788458c0cdca1996f6633c3080e5f82c
Michael Guse <guse.michael@gmail.com>
2024-08-19 16:30:13 -0500

    Created package.xml for Aurora Dev for testing

---
+++ crma_Deloitte_DEV_extract.sh
@@ -0,0 +1,44 @@
+#!/bin/zsh
+
+# Get Access Token from SF CLI environment
+sf org display --target-org mguse@deloitte.com.gcrm.6335400.aurora > crma_org.info
+token=`grep "Access Token" crma_org.info | cut -w -f 4`
+rm crma_org.info
+
+printf "\nExtracting CRMA Dashboard Info ... "
+
+curl -s https://deloitte--aurora.sandbox.my.salesforce.com/services/data/v58.0/wave/dashboards \
+    -H "Accept: application/json" \
+    -H "Authorization: Bearer $token" \
+    -o dashboards.json \
+
+jq '.dashboards[]|{Id: .id, Label: .label, CreatedDate: .createdDate, LastModDate: .lastModifiedDate}' dashboards.json > dashboard_list.json  
+
+tmp=`grep '\"Id\":' dashboard_list.json|wc -l|cut -w -f 2`
+printf "${tmp} entries.\n"
+
+printf "Extracting CRMA Dataset Info   ... "
+
+curl -s https://deloitte--aurora.sandbox.my.salesforce.com/services/data/v58.0/wave/datasets \
+    -H "Accept: application/json" \
+    -H "Authorization: Bearer $token" \
+    -o datasets.json \
+
+jq '.datasets[]|{Id: .id, Name: .name, CreatedDate: .createdDate, LastModDate: .lastModifiedDate}' datasets.json > dataset_list.json
+
+tmp=`grep '\"Id\":' dataset_list.json|wc -l|cut -w -f 2`
+printf "${tmp} entries.\n"
+
+printf "Extracting CRMA Lenses Info    ... "
+
+curl -s https://deloitte--aurora.sandbox.my.salesforce.com/services/data/v58.0/wave/lenses \
+    -H "Accept: application/json" \
+    -H "Authorization: Bearer $token" \
+    -o lenses.json \
+
+jq '.lenses[]|{Id: .id, Label: .label, CreatedDate: .createdDate, LastModDate: .lastModifiedDate}' lenses.json > lenses_list.json
+
+tmp=`grep '\"Id\":' lenses_list.json|wc -l|cut -w -f 2`
+printf "${tmp} entries.\n"
+
+printf "\nDone!\n"
