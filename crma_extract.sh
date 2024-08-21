52517e489bf7e8051e6e00355952f8cc1305e3f0
Michael Guse <guse.michael@gmail.com>
2024-08-21 10:20:55 -0500

    Added default values for DEV environment

--- crma_extract.sh
+++ crma_extract.sh
@@ -1,12 +1,13 @@
 #!/bin/zsh
 
-local inst=""
-local targetorg=""
+local inst="https://deloitte--aurora.sandbox.my.salesforce.com"
+local targetorg="mguse@deloitte.com.gcrm.6335400.aurora"
+local sfversion="61.0"
 local positional=()
 local debug="false"
 
 local usage=(
-    "crma_extract.sh [-h|--help] [-d|--debug] [-i|--inst <instance UR>] [-t|--targetorg <login email for targetorg>]"
+    "crma_extract.sh [-h|--help] [-d|--debug] [-i|--inst <instance UR>] [-t|--targetorg <login email for targetorg>] [-v|--version <Salesforce API Version|default('61.0')>]"
 )
 
 opterr() { echo >&2 "optparsing_demo: Unknown option '$1'" }
@@ -26,57 +27,55 @@ done
 
 if [[ $debug == "true" ]] 
 then
-    echo "Debug Info"
-    echo "--------------------------------"
-    echo "Instance URL: ${inst}"
-    echo "Target Org: ${targetorg}"
-    
-    echo "\n"
-    echo 'Press any key to continue...'; read -k1 -s
-else
-
-    # Get Access Token from SF CLI environment
-    #sf org display --target-org mguse@deloitte.com.gcrm.6335400.aurora > crma_org.info
-    sf org display --target-org ${targetorg} > crma_org.info
-    token=`grep "Access Token" crma_org.info | cut -w -f 4`
-    rm crma_org.info
-
-    printf "\nExtracting CRMA Dashboard Info ... "
-
-    curl -s ${inst}/services/data/v58.0/wave/dashboards \
-        -H "Accept: application/json" \
-        -H "Authorization: Bearer $token" \
-        -o dashboards.json \
-
-    jq '.dashboards[]|{Id: .id, Label: .label, Name: .name, CreatedDate: .createdDate, LastModDate: .lastModifiedDate}' dashboards.json > dashboard_list.json  
-
-    tmp=`grep '\"Id\":' dashboard_list.json|wc -l|cut -w -f 2`
-    printf "${tmp} entries.\n"
-
-    printf "Extracting CRMA Dataset Info   ... "
-
-    curl -s ${inst}/services/data/v58.0/wave/datasets \
-        -H "Accept: application/json" \
-        -H "Authorization: Bearer $token" \
-        -o datasets.json \
-
-    jq '.datasets[]|{Id: .id, Name: .name, CreatedDate: .createdDate, LastModDate: .lastModifiedDate}' datasets.json > dataset_list.json
-
-    tmp=`grep '\"Id\":' dataset_list.json|wc -l|cut -w -f 2`
-    printf "${tmp} entries.\n"
-
-    printf "Extracting CRMA Lenses Info    ... "
-
-    curl -s ${inst}/services/data/v58.0/wave/lenses \
-        -H "Accept: application/json" \
-        -H "Authorization: Bearer $token" \
-        -o lenses.json \
-
-    jq '.lenses[]|{Id: .id, Label: .label, Name: .name, CreatedDate: .createdDate, LastModDate: .lastModifiedDate}' lenses.json > lenses_list.json
-
-    tmp=`grep '\"Id\":' lenses_list.json|wc -l|cut -w -f 2`
-    printf "${tmp} entries.\n"
-
-    printf "\nDone!\n"
-
+    echo "\nDebug Info"
+    echo "----------------------------------------------------------------"
+    echo "Instance URL:       ${inst}"
+    echo "Target Org:         ${targetorg}"
+    echo "Salesforce Version: v${sfversion}"
+    echo '\nPress any key to continue...'; read -k1 -s
+    echo
 fi
+
+# Get Access Token from SF CLI environment
+#sf org display --target-org mguse@deloitte.com.gcrm.6335400.aurora > crma_org.info
+sf org display --target-org ${targetorg} > crma_org.info
+token=`grep "Access Token" crma_org.info | cut -w -f 4`
+rm crma_org.info
+
+printf "\nExtracting CRMA Dashboard Info ... "
+
+curl -s ${inst}/services/data/v${sfversion}/wave/dashboards \
+    -H "Accept: application/json" \
+    -H "Authorization: Bearer $token" \
+    -o dashboards.json \
+
+jq '.dashboards[]|{Id: .id, Label: .label, Name: .name, CreatedDate: .createdDate, LastModDate: .lastModifiedDate}' dashboards.json > dashboard_list.json  
+
+tmp=`grep '\"Id\":' dashboard_list.json|wc -l|cut -w -f 2`
+printf "${tmp} entries.\n"
+
+printf "Extracting CRMA Dataset Info   ... "
+
+curl -s ${inst}/services/data/v${sfversion}/wave/datasets \
+    -H "Accept: application/json" \
+    -H "Authorization: Bearer $token" \
+    -o datasets.json \
+
+jq '.datasets[]|{Id: .id, Name: .name, CreatedDate: .createdDate, LastModDate: .lastModifiedDate}' datasets.json > dataset_list.json
+
+tmp=`grep '\"Id\":' dataset_list.json|wc -l|cut -w -f 2`
+printf "${tmp} entries.\n"
+
+printf "Extracting CRMA Lenses Info    ... "
+
+curl -s ${inst}/services/data/v${sfversion}/wave/lenses \
+    -H "Accept: application/json" \
+    -H "Authorization: Bearer $token" \
+    -o lenses.json \
+
+jq '.lenses[]|{Id: .id, Label: .label, Name: .name, CreatedDate: .createdDate, LastModDate: .lastModifiedDate}' lenses.json > lenses_list.json
+
+tmp=`grep '\"Id\":' lenses_list.json|wc -l|cut -w -f 2`
+printf "${tmp} entries.\n"
+
+printf "\nDone!\n"
